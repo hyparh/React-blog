0cdf41394bcab46828045cccc94beb8c5cd9694c
hyparh <hyparh@gmail.com>
2022-09-10 19:38:15 +0300

    Added CSS for top panel

--- src/topbar/TopBar.jsx
+++ src/topbar/TopBar.jsx
@@ -4,13 +4,27 @@ const TopBar = () => {
   return (
     <div className="top">
       <div className="topLeft">
-        <i class="fa-brands fa-facebook"></i>
+        <i className="topIcon fa-brands fa-facebook"></i>
+        <i className="topIcon fa-brands fa-square-twitter"></i>
+        <i className="topIcon fa-brands fa-pinterest"></i>
+        <i className="topIcon fa-brands fa-square-instagram"></i>
       </div>
       <div className="topCenter">
-        <i class="fa-brands fa-square-twitter"></i>
+        <ul className="topList">
+          <li className="topListItem">HOME</li>
+          <li className="topListItem">ABOUT</li>
+          <li className="topListItem">CONTACT</li>
+          <li className="topListItem">WRITE</li>
+          <li className="topListItem">LOGOUT</li>
+        </ul>
       </div>
       <div className="topRight">
-        <i class="fa-brands fa-pinterest"></i>
+        <img
+          className="topImg"
+          src="https://upload.wikimedia.org/wikipedia/commons/b/b9/Yggdrasil.jpg"
+          alt=""
+        />
+        <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
       </div>
     </div>
   );
