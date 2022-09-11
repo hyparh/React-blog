f3eef416d1d9b0cb3447105e747f620ead0dfec6
hyparh <hyparh@gmail.com>
2022-09-11 22:08:43 +0300

    Created models and routes

---
+++ api/routes/users.js
@@ -0,0 +1,44 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+// UPDATE
+router.put("/:id", async (req, res) => {
+    if (req.body.userId === req.params.id) {
+        if (req.body.password) {
+            const salt = await bcrypt.genSalt(10);
+            req.body.password = await bcrypt.hash(req.body.password, salt);
+        }
+        try {
+            const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+                $set: req.body
+            }, { new: true });
+            res.status(200).json(updatedUser);
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    } else {
+        res.status(401).json("You can update only your account!");
+    }
+});
+
+// DELETE
+router.delete("/:id", async (req, res) => {
+    if (req.body.userId === req.params.id) {
+        try {
+            
+        } catch (error) {
+            
+        }
+        try {
+            await User.findByIdAndDelete(req.params.id);
+            res.status(200).json("User has been deleted.");
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    } else {
+        res.status(401).json("You can delete only your account!");
+    }
+});
+
+module.exports = router;
