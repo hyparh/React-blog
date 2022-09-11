99be3f6a3b99b26f13d4c1f6bcdcd427592df8c5
hyparh <hyparh@gmail.com>
2022-09-11 13:36:36 +0300

    Update Profile page ready

---
+++ src/pages/settings/Settings.jsx
@@ -0,0 +1,38 @@
+import "./settings.css";
+import Sidebar from "../../components/sidebar/Sidebar";
+
+const Settings = () => {
+  return (
+    <div className="settings">
+      <div className="settingsWrapper">
+        <div className="settingsTitle">
+          <span className="settingsUpdateTitle"> Update Your Account</span>
+          <span className="settingsDeleteTitle"> Delete Account</span>
+        </div>
+        <form className="settingsForm">
+          <label>Profile Picture</label>
+          <div className="settingsProfilePicture">
+            <img
+              src="http://3.bp.blogspot.com/_UQmvUodh4h4/TEOkir_PvaI/AAAAAAAADuU/7FuihWTvr0I/s1600/primordial+tree.jpg"
+              alt=""
+            />
+            <label htmlFor="fileInput">
+              <i className="settingsProfilePictureIcon fa-regular fa-user"></i>
+            </label>
+            <input type="file" id="fileInput" style={{display: "none"}} />
+          </div>
+          <label>Username</label>
+          <input type="text" placeholder="John" />
+          <label>Email</label>
+          <input type="email" placeholder="john@gmail.com" />
+          <label>Password</label>
+          <input type="password" />
+          <button className="settingsSubmit">Update</button>
+        </form>
+      </div>
+      <Sidebar />
+    </div>
+  );
+};
+
+export default Settings;
--- src/pages/write/Write.jsx
+++ src/pages/write/Write.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import "./write.css";
 
 const Write = () => {
   return (
-    <div>Write</div>
-  )
-}
+    <div className="write">
+      <img
+        src="https://www.cloudynights.com/uploads/monthly_09_2022/post-306923-0-13535800-1662853098.png"
+        alt=""
+        className="writeImg"
+      />
+      <form className="writeForm">
+        <div className="writeFormGroup">
+          <label htmlFor="fileInput">
+            <i className="writeIcon fas fa-plus"></i>
+          </label>
+          <input type="file" id="fileInput" style={{ display: "none" }} />
+          <input
+            type="text"
+            placeholder="Title"
+            className="writeInput"
+            autoFocus={true}
+          />
+        </div>
+        <div className="writeFormGroup">
+          <textarea
+            placeholder="Write description..."
+            type="text"
+            className="writeInput writeText"
+          ></textarea>
+        </div>
+        <button className="writeSubmit">Publish</button>
+      </form>
+    </div>
+  );
+};
 
 export default Write;
