b03bdc93c1e203ed4c42cb6599b03ff7ac7856ed
hyparh <hyparh@gmail.com>
2022-09-12 12:46:59 +0300

    Users routing implemented

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const Post = require("../models/Post");
 const bcrypt = require("bcrypt");
 
 // UPDATE
@@ -26,18 +27,30 @@ router.put("/:id", async (req, res) => {
 router.delete("/:id", async (req, res) => {
     if (req.body.userId === req.params.id) {
         try {
-            
+            const user = await User.findById(req.params.id);
+            try {
+                await Post.deleteMany({ username: user.username });
+                await User.findByIdAndDelete(req.params.id);
+                res.status(200).json("User has been deleted.");
+            } catch (error) {
+                res.status(500).json(error);
+            }
         } catch (error) {
-            
-        }
-        try {
-            await User.findByIdAndDelete(req.params.id);
-            res.status(200).json("User has been deleted.");
-        } catch (error) {
-            res.status(500).json(error);
+            res.status(404).json("User not found");
         }
     } else {
         res.status(401).json("You can delete only your account!");
+    }
+});
+
+// GET USER
+router.get("/:id", async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        const { password, ...others } = user._doc;
+        res.status(200).json(others);
+    } catch (error) {
+        res.status(500).json(error);
     }
 });
 
