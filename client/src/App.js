4d2bcb5d106ecceb6c42db08c6e2c36a2965a8e3
hyparh <hyparh@gmail.com>
2022-09-13 12:09:56 +0300

    Register, Login and Logout now work properly

--- client/src/App.js
+++ client/src/App.js
@@ -5,15 +5,17 @@ import Write from "./pages/write/Write";
 import Settings from "./pages/settings/Settings";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import { Context } from "./context/Context";
+import { useContext } from "react";
 import {
   BrowserRouter as Router,
   Routes,
-  Route, 
+  Route,
   Link
 } from "react-router-dom";
 
 function App() {
-  const user = false;
+  const { user } = useContext(Context);
 
   return (
     <Router>
--- client/src/components/topbar/TopBar.jsx
+++ client/src/components/topbar/TopBar.jsx
@@ -1,8 +1,13 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../../context/Context";
 import "./topbar.css";
 
 const TopBar = () => {
-  const user = false;
+  const { user, dispatch } = useContext(Context);
+  const handleLogout = () => {
+    dispatch({ type: "LOGOUT" });
+  };
 
   return (
     <div className="top">
@@ -34,10 +39,8 @@ const TopBar = () => {
               WRITE
             </Link>
           </li>
-          <li className="topListItem">
-            <Link className="link" to="/">
-              LOGOUT
-            </Link>
+          <li className="topListItem" onClick={handleLogout}>
+            {user && "LOGOUT"}
           </li>
         </ul>
       </div>
